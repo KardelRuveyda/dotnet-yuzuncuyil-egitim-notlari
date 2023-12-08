a394c6fa3aedcd5fd55ada1e7f551cf62c4f10c5
Kardel Ruveyda <33912144+KardelRuveyda@users.noreply.github.com>
2023-12-08 19:00:28 +0300

    Update Program.cs

--- Uygulamalar/design-patterns/DesignPatterns/Singleton/Program.cs
+++ Uygulamalar/design-patterns/DesignPatterns/Singleton/Program.cs
@@ -1,74 +1,47 @@
-﻿using System;
+//Bu nesneyi hiçbir zaman newleyerek oluşturamam
+// Bir kere üretildiğiden emin olacak şekilde üretmiş oluyoruz. 
+/*UserManager userManager = new UserManager();*///BÖYLE OLUŞTURAMAM
 
-// ILogger arayüzü, loglama işlemleri için bir şablon belirler.
-public interface ILogger
-{
-    void Log(string message);
-}
+//Aşağıdaki örnekle bir kere oluştuğundan emin oldum. 
+var userManager = UserManager.CreateAsSignleton();
+userManager.Save();
 
-// FileLogger, ILogger arayüzünü implemente ederek loglama işlemini dosyaya yapar.
-public class FileLogger : ILogger
+
+class UserManager
 {
-    public void Log(string message)
+    private  static UserManager _userManager;
+
+    //thread safe.
+    static object _lockObject = new object();
+    //Dış erişimi engellemek için private bir UserManager oluştururlur
+    private UserManager()
     {
-        Console.WriteLine($"Log to file: {message}");
-    }
-}
 
-// ConsoleLogger, ILogger arayüzünü implemente ederek loglama işlemini konsola yapar.
-public class ConsoleLogger : ILogger
-{
-    public void Log(string message)
-    {
-        Console.WriteLine($"Log to console: {message}");
-    }
-}
-
-// UserService, bir ILogger bağımlılığına sahip olan bir servis sınıfıdır.
-public class UserService
-{
-    private readonly ILogger _logger;
-
-    // Bağımlılık enjeksiyonu ile ILogger örneği alınır.
-    public UserService(ILogger logger)
-    {
-        _logger = logger;
     }
 
-    public void SaveUser(string username)
+    public static UserManager CreateAsSignleton()
     {
-        // Kullanıcı kaydedildiği zaman loglama işlemi yapılır.
-        _logger.Log($"User saved: {username}");
-        // Gerçek kullanıcı kaydetme işlemleri burada gerçekleştirilir.
+        //if(_userManager == null)
+        //{
+        //    _userManager = new UserManager();
+        //}
+
+        //return _userManager ?? (_userManager = new UserManager());
+
+        lock (_lockObject)
+        {
+            if (_userManager == null)
+            {
+                _userManager = new UserManager();
+            }
+        }
+
+        return _userManager;
+
+    }
+    //static yazarsam kullanamam. 
+    public void Save()
+    {
+        Console.WriteLine("Kayıt işlemi başarıyla kaydedildi.");
     }
 }
-
-class Program
-{
-    static void Main()
-    {
-        // Dependency Injection ile FileLogger kullanarak UserService örneği oluşturuluyor.
-        var userServiceWithFileLogger = new UserService(new FileLogger());
-        userServiceWithFileLogger.SaveUser("John Doe");
-
-        Console.WriteLine("----------------------");
-
-        // Dependency Injection ile ConsoleLogger kullanarak UserService örneği oluşturuluyor.
-        var userServiceWithConsoleLogger = new UserService(new ConsoleLogger());
-        userServiceWithConsoleLogger.SaveUser("Jane Doe");
-    }
-}
-
-/*
-Yukarıdaki örnekte ILogger arayüzü loglama işlemleri için bir şablon belirler. 
-    FileLogger ve ConsoleLogger sınıfları bu arayüzü implemente eder ve kendi loglama 
-    yöntemlerini tanımlarlar.
-
-UserService sınıfı, bir ILogger bağımlılığına sahiptir ve bu bağımlılık constructor 
-aracılığıyla enjekte edilir. Bu sayede, UserService sınıfı loglama işlemleri için 
-herhangi bir ILogger implementasyonuyla çalışabilir.
-
-Main metodu içinde, UserService sınıfının iki farklı örneği oluşturuluyor ve
-her birine farklı bir ILogger implementasyonu enjekte ediliyor. Bu şekilde 
-Dependency Injection sayesinde, UserService sınıfı loglama işlemleri için esnek ve
-değiştirilebilir hale gelir.*/
