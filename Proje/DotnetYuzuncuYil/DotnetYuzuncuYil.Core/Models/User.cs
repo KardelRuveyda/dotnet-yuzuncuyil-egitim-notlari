e4c2c47f85aed0027f431183f08a6a96f0630dfa
Ruveyda Cetin <ruveyda.cetin@d-teknoloji.com.tr>
2023-12-16 21:35:25 +0300

    Core katmanı geliştirmeleri tamamlandı.

--- Proje/DotnetYuzuncuYil/DotnetYuzuncuYil.Core/Models/User.cs
+++ Proje/DotnetYuzuncuYil/DotnetYuzuncuYil.Core/Models/User.cs
@@ -11,7 +11,7 @@ namespace DotnetYuzuncuYil.Core
     {
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Passsword { get; set; }
+        public string Password { get; set; }
         //İlişkilenidirme Kısmında
         //Foreign Key
         public int TeamId { get; set; }
